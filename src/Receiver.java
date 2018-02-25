3948c68671f48d205aeaa8607f562dedbd963f37
Richard Hugessen <richard@hugessen.ca>
2018-02-24 19:22:23 -0500

    Reconstruct file from datagrams

--- src/Receiver.java
+++ src/Receiver.java
@@ -13,17 +13,29 @@ public class Receiver {
     DatagramSocket receiverSocket = new DatagramSocket(receiverPort);
     DatagramSocket senderSocket = new DatagramSocket();
     System.out.println("Socket initiated");
+    
+    String file = new String("");
+    boolean finished = false;
 
-    while(true) {
+    while(!finished) {
       byte[] buf = new byte[256];
       DatagramPacket packet = new DatagramPacket(buf, buf.length);
       receiverSocket.receive(packet);
-      System.out.println("I got a packet!!!!!!!!");
+      System.out.println("Packet Received");
+      String str = new String(buf);
+      
+      if (str.trim().equals("DONE")) {
+        finished = true;
+      }
+      else {
+        file += str;
+      } 
 
       packet = new DatagramPacket(buf, buf.length, senderAddress, senderPort);
       senderSocket.send(packet);
       System.out.println("sending ACK to " + senderAddress + ":" + senderPort);
     }
+    System.out.println(file);
     
   }
 }
--- src/Sender.java
+++ src/Sender.java
@@ -12,7 +12,6 @@ public class Sender {
 
   public static void main(String[] richard) throws IOException {
     String[] args = {"Not currently used","8888","5555","test.txt","500"}; //Hardcoding this stuff for now
-    System.out.println("At least we got here yo. Little victories");
     
     if (args.length != 5) {
       System.out.println("Error: Wrong number of arguments");
@@ -20,7 +19,7 @@ public class Sender {
     }
     
     try {
-      in = new BufferedReader(new FileReader("test.txt"));
+      in = new BufferedReader(new FileReader("/Users/richard/eclipse-workspace/CP372-A2/src/test.txt"));
     } catch (FileNotFoundException e) {
       System.err.println("Could not open test file.");
     }
@@ -36,20 +35,19 @@ public class Sender {
     receiverSocket = new DatagramSocket();
     senderSocket = new DatagramSocket(senderPort);
 
-    char[] buf = new char[MAX_BYTES/2]; //Not sure if this is how he wants it, but a char is 2 bytes
+    char[] buf = new char[MAX_BYTES];
     int offset = 0;
 
-    // Keep trying to read MAX_BYTES/2 chars at a time until we hit EOF
-    while (in.read(buf,0,MAX_BYTES/2) != -1) 
+    // Keep trying to read MAX_BYTES chars at a time until we hit EOF
+    while (in.read(buf,0,MAX_BYTES) != -1) 
     {
       boolean ack = false;
       while (ack == false)
       {
-        String str = new String(buf);
-        byte[] bytes = str.getBytes(); // Will not always contain the full 124 bytes because smaller chars are encoded with only 1 byte.
-        //System.out.println("Length of byte buffer: " + bytes.length); //For testing
+        byte[] bytes = new String(buf).getBytes();
+        buf = new char[MAX_BYTES]; //Refresh the buffer
         DatagramPacket packet = new DatagramPacket(bytes, bytes.length, receiverAddress, receiverPort);
-  			System.out.println(bytes + " " + bytes.length + " " + receiverAddress + " " + receiverPort);
+		System.out.println(bytes + " " + bytes.length + " " + receiverAddress + " " + receiverPort);
         receiverSocket.send(packet);
         //try {
           //socket.setSoTimeout(timeout);
@@ -64,6 +62,11 @@ public class Sender {
         }*/
       }
     }
+    //Send termination message
+    byte[] bytes = "DONE".getBytes();
+    DatagramPacket packet = new DatagramPacket(bytes, bytes.length, receiverAddress, receiverPort);
+    receiverSocket.send(packet);
+    
     System.out.println("Done");
     in.close();
 
